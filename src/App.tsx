1e3975cc8e81f01443821b13c8ac68b61950a3d0
RabinThapa1998 <imrabin1998@gmail.com>
2022-09-27 17:40:01 +0545

    🎉 vite-ts-eslint-prettier-antd

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from '~/components/common/layout';
+import Dashboard from './containers/dashboard';
+
+function App() {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+}
+
+export default App;
